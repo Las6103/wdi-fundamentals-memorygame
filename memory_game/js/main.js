39acaa4c3ec79f435a796e130ad1494ea8512446
Leonardo Silva <lenny@Leonardos-MBP.fios-router.home>
2020-04-09 18:42:19 -0400

    created alerts and arrays

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,22 @@
-let cardOne = "queen";
-let cardTwo = "Kkng";
-let cardThree = "queen";
-let cardFour = "king";
+const cards = ["queen", "king", "queen", "king"];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+let cardsInPlay = [];
+
+let cardOne = cards[0];
+
+let cardTwo = cards[1];
+
+cardsInPlay.push(cardOne);
+
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry,try again.");
+  }
+}
+
+console.log("User flipped Queen");
+console.log("User flipped King");
